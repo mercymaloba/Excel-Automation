51b42a97395f7ed9e7fc444d4ab1bacccc646dd7
malobs <malobamercy958@gmail.com>
2023-09-11 22:53:36 +0300

    Add a column in excel workbook

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ cell= sheet['a1']
 cell= sheet.cell(1,1)
 
 
-for row in range(1, sheet.max_row + 1):
-    print(row)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    corrected_price = cell.value * int(9)
+    corrected_price_cell = sheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+wb.save('transactions2.xlsx')
+    
