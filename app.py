5cb3ab0f3e0deb2f7e2513c2b86cf6d70755a7c4
malobs <malobamercy958@gmail.com>
2023-09-13 22:52:45 +0300

    creating function

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook('transactions.xlsx')
-sheet = wb['Sheet1']
+def process_workbook(filename):
 
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    
-    # Check if the cell's value can be converted to a float
-    try:
-        cell_value = float(cell.value)
-    except (ValueError, TypeError):
-        # Handle non-numeric values or errors by printing and skipping
-        print(f"Skipping row {row}: Value '{cell.value}' is not numeric.")
-        continue
-    
-    corrected_price = (float(cell.value) * 0.9)
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-values = Reference(sheet, 
-          min_row=2, 
-          max_row=sheet.max_row,
-          min_col=4,
-          max_col=4)
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        
+        # Check if the cell's value can be converted to a float
+        try:
+            cell_value = float(cell.value)
+        except (ValueError, TypeError):
+            # Handle non-numeric values or errors by printing and skipping
+            print(f"Skipping row {row}: Value '{cell.value}' is not numeric.")
+            continue
+        
+        corrected_price = (float(cell.value) * 0.9)
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
 
-chart= BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    values = Reference(sheet, 
+            min_row=2, 
+            max_row=sheet.max_row,
+            min_col=4,
+            max_col=4)
 
-wb.save('transactions2.xlsx')
+    chart= BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
 
 
 
